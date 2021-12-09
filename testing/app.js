3598417d494a456ef2256347e63624b1e77032f4
Tenison <tenisonoseiowusu@gmail.com>
2021-12-09 17:35:02 +0000

    unit testing intro updated

---
+++ testing/app.js
@@ -0,0 +1,4 @@
+/**
+ * Entry point for app
+ * But this file is not used because we are just running unit test for independent js modules eg: unit_test_example.js
+ */
--- testing/unit_test_example.spec.js
+++ testing/unit_test_example.spec.js
@@ -6,3 +6,18 @@ test('sum function test', () => {
     expect(sum(2,1)).toBe(3)
     expect(sum(-5,1)).toBe(-4)
 })
+
+
+test('subtract function test', () => {
+    expect(subtract(2,1)).toBe(1)
+    expect(subtract(-5,1)).toBe(-6)
+    expect(subtract(0,1)).toBe(-1)
+})
+
+
+test('divide function test', () => {
+    expect(divide(9,3)).toBe(3)
+    expect(divide(-4,1)).toBe(-4)
+    expect(divide(12,3)).toBe(4)
+    expect(divide(0,12)).toBe(0)
+})
